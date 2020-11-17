b5c9e6f324f93691bb0a5a21127699ed7db39033
asorokin <a40in@inbox.ru>
2020-11-17 16:10:23 +0200

    create connector SSH/SFTP

---
+++ connect.go
@@ -0,0 +1,129 @@
+package connector
+
+import (
+	"errors"
+	"io"
+	"io/ioutil"
+	"os"
+	"time"
+
+	"github.com/pkg/sftp"
+	"golang.org/x/crypto/ssh"
+)
+
+// Credentials ...AuthMethod : "key", "password", "keyboard". Port default ":22"
+type Credentials struct {
+	Host           string
+	Port           string
+	AuthMethod     string
+	User           string
+	Password       string
+	PrivateKeyFile string
+	TimeOut        int64
+}
+
+// SSH ...
+type SSH struct {
+	client *ssh.Client
+}
+
+// NewSSH ...
+func NewSSH(c *Credentials) (*SSH, error) {
+
+	cfg := &ssh.ClientConfig{
+
+		User:            c.User,
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+		Timeout:         time.Duration(c.TimeOut) * time.Second,
+	}
+
+	switch c.AuthMethod {
+	case "key":
+		privateKey, err := ioutil.ReadFile(c.PrivateKeyFile)
+		if err != nil {
+			return nil, err
+		}
+		signer, err := ssh.ParsePrivateKey(privateKey)
+		if err != nil {
+			return nil, err
+		}
+		cfg.Auth = []ssh.AuthMethod{
+			ssh.PublicKeys(signer),
+		}
+	case "password":
+		cfg.Auth = []ssh.AuthMethod{
+			ssh.Password(c.Password),
+		}
+	case "keyboard":
+		cfg.Auth = []ssh.AuthMethod{
+			ssh.KeyboardInteractive(func(user, instruction string, questions []string, echos []bool) ([]string, error) {
+				// Just sends the password back for all questions
+				answers := make([]string, len(questions))
+				for i := range answers {
+					answers[i] = c.Password
+				}
+				return answers, nil
+			}),
+		}
+	default:
+		err := errors.New("unsupported authentication method")
+		return nil, err
+	}
+
+	if c.Port == "" {
+		c.Port = "22"
+	}
+
+	client, err := ssh.Dial("tcp", c.Host+":"+c.Port, cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return &SSH{client: client}, nil
+}
+
+// ClientSFTP ...
+func (s *SSH) ClientSFTP() (*sftp.Client, error) {
+	client, err := sftp.NewClient(s.client)
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
+
+// NewSession ...
+func (s *SSH) NewSession() (*ssh.Session, error) {
+	session, err := s.client.NewSession()
+	if err != nil {
+		return nil, err
+	}
+
+	return session, nil
+}
+
+// RunCommand ...
+// TODO: дописать выполнение команд с возвратом результатов
+func RunCommand(session *ssh.Session, cmd string) error {
+	sessStdOut, err := session.StdoutPipe()
+	if err != nil {
+		return err
+	}
+	go io.Copy(os.Stdout, sessStdOut)
+	sessStderr, err := session.StderrPipe()
+	if err != nil {
+		return err
+	}
+	go io.Copy(os.Stderr, sessStderr)
+	err = session.Run(cmd) // eg., /usr/bin/whoami
+	if err != nil {
+		return err
+	}
+	// var stdoutBuf bytes.Buffer
+	// session.Stdout = &stdoutBuf
+	// session.Run(cmd)
+
+	// return hostname + ": " + stdoutBuf.String()
+
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/NuclearLouse/utilities-connectSSH_SFTP
+
+go 1.15
+
+require (
+	github.com/pkg/sftp v1.12.0
+	golang.org/x/crypto v0.0.0-20201116153603-4be66e5b6582
+)
